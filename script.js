d53e107d745e7ca94c08fcbf89fc73a3660f8be9
Shirley Wang <shirleywang527@gmail.com>
2019-04-03 07:46:51 +0200

    a little more housekeeping

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
-// toggle scroll top arrow when it hits #about
+const cartBtn = document.querySelector('.cart-info');
+const cart = document.querySelector('.cart');
+
 const scrollTop = document.querySelector('.gotopbtn');
 const aboutSec = document.querySelector('#about');
 const topOfAbout = aboutSec.offsetTop;
 
+// display shopping cart
+cartBtn.addEventListener('click', () => {
+    cart.classList.add('show-cart');
+});
+
+// click outside the shopping cart to hide it
+document.addEventListener('click', (e) => {
+    if(!e.target.closest('.cart'))
+    // console.log('click');
+    return;
+
+    cart.classList.toggle('show-cart')
+});
+
+// toggle scroll top arrow when it hits #about
 function toTop() {
     (window.scrollY >= topOfAbout) ? scrollTop.classList.remove('hidden') : scrollTop.classList.add('hidden');
 }
