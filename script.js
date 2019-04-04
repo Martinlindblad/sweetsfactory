edf3ae65ce432402ebd714f26fc03ec79ed8f628
Shirley Wang <shirleywang527@gmail.com>
2019-04-04 21:53:44 +0200

    add some trivial stuff

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// global variables
 const scrollTop = document.querySelector('.gotopbtn');
 const aboutSec = document.querySelector('#about');
 const topOfAbout = aboutSec.offsetTop;
@@ -8,6 +9,26 @@ const topOfAbout = aboutSec.offsetTop;
     const cart = document.querySelector('.cart');
 
     cartInfo.addEventListener('click', () => cart.classList.toggle('show-cart'));
+})();
+
+// add items to cart
+(function () {
+    const cartBtn = document.querySelectorAll('.store-item-icon');
+
+    cartBtn.forEach(function(btn) {
+        btn.addEventListener('click', (e) => {
+            // console.log(e.target);
+
+            if(e.target.parentElement.classList.contains('store-item-icon')) {
+                const cartItem = document.createElement('div');
+
+                cartItem.classList.add('cart-item', 'd-flex', 'text-captalize', 'my-3');
+                cartItem.innerHTML = `
+                    <img src="">
+                `;
+            }
+        });
+    });
 })();
 
 // toggle scroll top arrow when it hits #about
