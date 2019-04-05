8af97f129c4733918a7ce5a61ed212d2727a3859
MartinLindblad <martin.lindblad1@gmail.com>
2019-04-05 22:16:59 +0200

    no message

--- modal-category/category/category.js
+++ modal-category/category/category.js
@@ -12,7 +12,7 @@ setInterval(() => {
 }, 24800);
 
 function bannerRoll(){       
-    let high = $('.img1').height();      // get the hight for one img
+    let high = $('.img1').height();      // get the hight from one img
     high = high * 5;    // all images have the same size. 6 images. there fore img height*5
     
     $(".banner-roll").stop(true,true).animate({scrollTop: high}, 12000, "linear" ,  // Start the action, will move the page to "high"
