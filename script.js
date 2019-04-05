4004e832bf53a5b7e59829f7a3edddf142577bd5
Shirley Wang <shirleywang527@gmail.com>
2019-04-05 10:03:30 +0200

    modify shopping cart

--- script.js
+++ script.js
@@ -1,7 +1,33 @@
-// global variables
-const scrollTop = document.querySelector('.gotopbtn');
-const aboutSec = document.querySelector('#about');
-const topOfAbout = aboutSec.offsetTop;
+// remove items from the cart
+const removeCartItemBtns = document.getElementsByClassName('cart-item-remove');
+// console.log(removeCartItemBtns);
+for(let i = 0; i < removeCartItemBtns.length; i++) {
+    const btn = removeCartItemBtns[i];
+    btn.addEventListener('click', (e) => {
+        const btnClick = e.target;
+        btnClick.parentElement.parentElement.remove();
+        updateTotal();
+    });
+}
+
+function updateTotal() {
+    const cartItemContainer = document.getElementsByClassName('cart')[0]
+    const cartRows = cartItemContainer.getElementsByClassName('cart-item');
+    let total = 0;
+
+    for(let i = 0; i < cartRows.length; i++) {
+        const cartRow = cartRows[i];
+        const priceEl = cartRow.getElementsByClassName('cart-item-price')[0];
+        const qtyEl = cartRow.getElementsByClassName('cart-quantity-input')[0];
+
+        const price = priceEl.innerText;
+        const qty = qtyEl.value;
+        total = total + (price * qty);
+    }
+    document.getElementsByClassName('cart-total-price')[0].innerText = total;
+    console.log(total);
+}
+// updateTotal();
 
 // toggle shopping cart
 (function() {
@@ -12,27 +38,31 @@ const topOfAbout = aboutSec.offsetTop;
 })();
 
 // add items to cart
-(function () {
-    const cartBtn = document.querySelectorAll('.store-item-icon');
+// (function () {
+//     const cartBtn = document.querySelectorAll('.store-item-icon');
 
-    cartBtn.forEach(function(btn) {
-        btn.addEventListener('click', (e) => {
-            // console.log(e.target);
+//     cartBtn.forEach(function(btn) {
+//         btn.addEventListener('click', (e) => {
+//             // console.log(e.target);
 
-            if(e.target.parentElement.classList.contains('store-item-icon')) {
-                const cartItem = document.createElement('div');
+//             if(e.target.parentElement.classList.contains('store-item-icon')) {
+//                 const cartItem = document.createElement('div');
 
-                cartItem.classList.add('cart-item', 'd-flex', 'text-captalize', 'my-3');
-                cartItem.innerHTML = `
-                    <img src="">
-                `;
-            }
-        });
-    });
-})();
+//                 cartItem.classList.add('cart-item', 'd-flex', 'text-captalize', 'my-3');
+//                 cartItem.innerHTML = `
+//                 <img src="">
+//                 `;
+//             }
+//         });
+//     });
+// })();
 
 // toggle scroll top arrow when it hits #about
 function toTop() {
+    const scrollTop = document.querySelector('.gotopbtn');
+    const aboutSec = document.querySelector('#about');
+    const topOfAbout = aboutSec.offsetTop;
+
     (window.scrollY >= topOfAbout) ? scrollTop.classList.remove('hidden') : scrollTop.classList.add('hidden');
 }
 
