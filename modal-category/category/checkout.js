a9fdaae272876c9ba0de05212e6b74c1bfdc417d
tocico <kunotoshiko17@gmail.com>
2019-04-06 23:23:18 +0200

    Fixed confirm page

--- modal-category/category/checkout.js
+++ modal-category/category/checkout.js
@@ -1,203 +1,17 @@
-function ready() {
-    // remove items from the cart
-    const removeCartItemBtns = document.getElementsByClassName('cart-item-remove');
-    // console.log(removeCartItemBtns);
-    for(let i = 0; i < removeCartItemBtns.length; i++) {
-        const btn = removeCartItemBtns[i];
-        btn.addEventListener('click', removeCartItems);
-    }
-
-    const qtyInput = document.getElementsByClassName('cart-quantity-input')
-    for(let i = 0; i < qtyInput.length; i++) {
-        const input = qtyInput[i]
-        input.addEventListener('change', qtyChanged)
-    }
-
-    const addToCartBtn = document.getElementsByClassName('store-item-icon')
-    for(let i = 0; i < addToCartBtn.length; i++) {
-        const btn = addToCartBtn[i]
-        btn.addEventListener('click', addToCartClick)
-    }
-
-    document.querySelector('.btn-checkout').addEventListener('click', checkoutClick)
-}
-
-ready();
-
-function checkoutClick() {
-    alert('Thanks for your purchase. Have a nice day!')
-    const allItems = document.getElementsByClassName('cart-items')[0]
-    while(allItems.hasChildNodes()) {
-        allItems.removeChild(allItems.firstChild)
-    }
-    updateTotal()
-}
-
-function removeCartItems(e) {
-    const btnClick = e.target;
-    btnClick.parentElement.parentElement.remove();
-    updateTotal();
-}
-
-function qtyChanged(e) {
-    const input = e.target
-    if(isNaN(input.value) || input.value <= 0) {
-        input.value = 1;
-    }
-    updateTotal()
-}
-
-function addToCartClick(e) {
-    const btn = e.target
-    const cardItem = btn.parentElement
-    const cardTxt = cardItem.getElementsByClassName('card-text')[0].innerText
-    const price = cardItem.getElementsByClassName('cart-item-price')[0].innerText
-    const imageSrc = cardItem.getElementsByClassName('card-img')[0].src
-
-    console.log(cardTxt, price, imageSrc);
-    addItemToCart(cardTxt, price, imageSrc);
-    updateTotal();
-}
-
-function addItemToCart(cardTxt, price, imageSrc) {
-    const cartRow = document.createElement('div')
-    const cartItems = document.getElementsByClassName('cart-items')[0]
-    const cartItemNames = document.getElementsByClassName('item-text')
-
-    for(let i = 0; i < cartItemNames.length; i++) {
-        if(cartItemNames[i].innerText === cardTxt) {
-            alert('Item has already been added')
-            return
-        }
-    }
-    const cartRowContent = `
-        <div class="cart-item d-flex align-items-center justify-content-between">
-            <img src="${imageSrc}" class="img-fluid card-img" id="item-img" alt="">
-            <span class="item-text mx-5">${cardTxt}</span>
-            <span>$</span>
-            <span id="cart-item-price" class="cart-item-price" class="mb-0">${price}</span>
-            <input type="number" value="1" class="cart-quantity-input">
-            <button id='cart-item-remove' class="btn cart-item-remove my-auto">
-                <i class="fas fa-trash"></i>
-            </button>
-        </div>
-    `;
-    cartRow.innerHTML = cartRowContent
-
-    cartItems.append(cartRow)
-
-    cartRow.getElementsByClassName('cart-item-remove')[0].addEventListener('click', removeCartItems)
-
-    cartRow.getElementsByClassName('cart-quantity-input')[0].addEventListener('change', qtyChanged)
-}
-
-function updateTotal() {
-    const cartItemContainer = document.getElementsByClassName('cart')[0]
-    const cartRows = cartItemContainer.getElementsByClassName('cart-item');
-    let total = 0;
-
-    for(let i = 0; i < cartRows.length; i++) {
-        const cartRow = cartRows[i];
-        const priceEl = cartRow.getElementsByClassName('cart-item-price')[0];
-        const qtyEl = cartRow.getElementsByClassName('cart-quantity-input')[0];
-
-        const price = priceEl.innerText;
-        const qty = qtyEl.value;
-        total = total + (price * qty);
-    }
-    document.getElementsByClassName('cart-total-price')[0].innerText = total;
-
-    // always get only 2 decimals
-    total = Math.round(total * 100) / 100
-    // console.log(total);
-}
-
 
 /*------------------ toggle shopping cart -------------------*/
 
-(function () {
-    const cartInfo = document.querySelector('.cart-info');
-    const cart = document.querySelector('.cart');
+// (function () {
+//     const cartInfo = document.querySelector('.cart-info');
+//     const cart = document.querySelector('.cart');
 
-    cartInfo.addEventListener('click', () => cart.classList.toggle('show-cart'));
-})();
+//     cartInfo.addEventListener('click', () => cart.classList.toggle('show-cart'));
+// })();
 
-
-
-/*-----------modal window-----------------*/
-
-
-// $(function () {
-// let btn = $('button[data-toggle="modal"]');
-// let showAmout = $('.product-price'); // Show price on modal
-// let quantity = $('#show-num'); // Show antal styck on modal
-// let image = $('.cart-item-image');
-// let productName = $(".modal-title");
-// let totalAmount = 0; // total summan  
-// let defaultq = [];
-
-
-
-// // När man klickar BUY btn
-// btn.on('click', function () {
-//     let title = $(this).parents()[0].offsetParent.childNodes[3].innerText; // Product name
-//     let amount = $(this).siblings()[0].firstElementChild.innerHTML; // price each
-//     let img = $(this).parents()[0].offsetParent.children[0].attributes[0].nodeValue; // image
-//     productName.html(title);
-//     image.attr('src', img);
-//     console.log(amount);
-//     showAmout.html(amount); // Visa produktens original pris
-//     quantity.html(1); // Visa 1 på antal fältet
-//     plus(amount);
-//     minus(amount);
-// });
-
-
-// /*-------------Count and calculate------------*/
-// /*--------plus function-----*/
-// function plus(p) {
-//     $('#plus').on('click', function (e) {
-//         e.preventDefault();
-//         defaultq = parseInt($(this).siblings()[1].innerHTML);
-//         console.log(defaultq);
-//         defaultq += 1;
-//         quantity.html(defaultq).val();
-//         // console.log(quantity[0].innerHTML); // visa antal
-//         totalAmount = defaultq * p;
-//         // console.log(totalAmount);
-//         showPrice(totalAmount);
-//     });
-// }
-// /*------minus function-----*/
-// function minus(p) {
-//     $('#minus').on('click', function (e) {
-//         e.preventDefault();
-//         defaultq = parseInt($(this).siblings()[0].innerHTML);
-//         if (defaultq > 1) {
-//             defaultq -= 1;
-//             quantity.text(defaultq).val(); // visa antal
-//             totalAmount = totalAmount - p;
-//             showPrice(totalAmount);
-//             console.log(defaultq);
-//         }
-//         else {
-//             defaultq = 1;
-//             showAmout.html(p);
-//             console.log(defaultq);
-//         }
-//     });
-// }
-
-// /*-------------visa pris -----------------*/
-// function showPrice(total) {
-//     showAmout.html(total.toFixed(2));
-// }
-
-//}); // ready
 
 /*---------------- checkout validation ---------------*/
 $(function () {
-    
+
     // Check firstname, lastname, address and city form
     $('#firstname,#lastname,#address,#city').on('blur', function () {
         // $(this).addClass('bg');
@@ -211,7 +25,7 @@ $(function () {
             $(this).attr('data-valid', 'true');
         }
     });
-    
+
     // Check Email form
     let mailerror = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     $('#mail').on('blur', function () {
@@ -224,7 +38,7 @@ $(function () {
             $(this).prev().fadeIn().html("Check your email address");
             $(this).attr('data-valid', 'false');
         }
-        else if(mail !== ''){
+        else if (mail !== '') {
             $(this).prev().fadeOut();
             $(this).attr('data-valid', 'true');
         }
@@ -259,24 +73,98 @@ $(function () {
             $(this).prev().fadeIn().html("Check your postcode");
             $(this).attr('data-valid', 'false');
         }
-        else if(postcode !== ''){
+        else if (postcode !== '') {
             $(this).prev().fadeOut();
             $(this).attr('data-valid', 'true');
         }
     });
 
-    
+
+    //Loopar genom alla input fältet om de är fyllda
+    //kollar om attribute data-valid = true eller false
+    //Om det är false då visar man fel meddelande
+    //Om alla är godkända då visas order confirm
+
     $('button[type="submit"]').on('click', function (e) {
         e.preventDefault();
-        if($(this).attr('data-valid', 'true')){
-            console.log('true');
+        let trueCount = 0; // räknar hur många true
+        let falseCount = 0; // räkn
+        $("input").each(function (i) {
+            let valid = $(this).attr('data-valid');
+            if (valid === 'true') {
+                $(this).prev().fadeOut(); // Ingen error message
+                trueCount++;
+            }
+            else if (valid === 'false') {
+                $(this).prev().fadeIn(); // Visa error message
+                falseCount++;
+            }
+        });
+        // Här kollar hur många trueCount och falseCount
+        //Om alla 7 fältet är fyllda då gå vidare till modalSubmit() funktion vilket är  
+        if (trueCount === 7) {
+            false;
+            modalSubmit();
         }
-        else{
-            console.log('false');
+        else if (falseCount < 7) {
+            true;
         }
+
     });
 
+});
 
-})
+
+
+/*-------- Show modal order confirm page ---------------*/
+function modalSubmit() {
+    // Lägga till attribute värde #check-modal för att kunna öppna modal form
+    $('button[type="submit"]').attr('data-target', '#check-modal');
+
+    // Visa de valda produkterna via locakstorage
+    getItem();
+
+    function getItem() {
+
+    }
+
+
+    // caching modal ID av personal infomation
+    let firstname = $('#firstName');
+    let lasttname = $('#lastName');
+    let email = $('#eMail');
+    let tel = $('#telNmb');
+    let address = $('#addRess');
+    let cityname = $('#cityName');
+    let postcode = $('#postCode');
+
+    //Visa värdet på order confirm sidan
+    firstname.html($('#firstname').val());
+    lasttname.html($('#lastname').val());
+    email.html($('#mail').val());
+    tel.html($('#tel').val());
+    address.html($('#address').val());
+    cityname.html($('#city').val());
+    postcode.html($('#postcode').val());
+
+    
+    // Lagra information i localStorage
+    let personInfoArray = [
+        ($('#firstname').val()),
+        ($('#lastname').val()),
+        ($('#mail').val()),
+        ($('#tel').val()),
+        ($('#address').val()),
+        ($('#city').val()),
+        ($('#postcode').val())
+    ];
+    //Konvertera en array till en JSON innan lagrar i LS
+    JSON.stringify(personInfoArray);
+    localStorage.setItem('personInfoArray', personInfoArray);
+    console.table(localStorage);
+
+}
+
+
 
 
